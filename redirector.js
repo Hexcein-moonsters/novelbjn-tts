5c91553c1ba71e9c13965690644ef4475cfc86e8
Chrome Extensions <123759385+Hexcein-moonsters@users.noreply.github.com>
2024-07-16 11:40:31 +0200

    Add support for /info

--- redirector.js
+++ redirector.js
@@ -3,15 +3,8 @@ const originalFetch = window.fetch;
 window.fetch = function(input, init) {
     if (typeof input === 'string' && input.startsWith('https://tonyassi-voice-clone.hf.space/config')) {
         input = './tonyassi-voice-clone-config.json';
+    } else if (typeof input === 'string' && input.startsWith('https://tonyassi-voice-clone.hf.space/info')) {
+        input = './tonyassi-voice-clone-info.json'
     }
     return originalFetch(input, init);
 };
-
-// // Override the native XMLHttpRequest to intercept network requests
-// const originalXhrOpen = XMLHttpRequest.prototype.open;
-// XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
-//     if (url.startsWith('https://api.dashcraft.io/trackv2/65bd295e635608c4d25bf536')) {
-//         url = 'https://api.dashcraft.io/trackv2';
-//     }
-//     return originalXhrOpen.call(this, method, url, async, user, password);
-// };
