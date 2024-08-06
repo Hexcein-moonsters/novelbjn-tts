29f32b8ceb780ac4c09dc810d0de02ded1e40706
Chrome Extensions <123759385+Hexcein-moonsters@users.noreply.github.com>
2024-08-06 11:08:21 +0200

    Correct text+novel input num

--- index.js
+++ index.js
@@ -78,3 +78,24 @@ async function text_to_speech(text) {
 	]);
     console.log(result.data);
 }
+
+
+
+
+
+
+
+function copyNovel() {
+    var copyText = document.getElementById("myInput");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+    
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copied: " + copyText.value;
+}
+  
+function outFunc() {
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copy to clipboard";
+}
